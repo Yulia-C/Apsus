c467db0e77728c6f033202635fd36ca5ecf3217e
Yulia C <yulka1355@gmail.com>
2025-06-30 20:38:58 +0300

    Collaboration commit trial

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -5,6 +5,7 @@ export function AppHeader() {
     return <header className="app-header">
         <Link to="/">
             <h3>LOGO ✨</h3>
+            <h4>Another LOGO</h4>
         </Link>
         <nav>
             <NavLink to="/">Home</NavLink>
