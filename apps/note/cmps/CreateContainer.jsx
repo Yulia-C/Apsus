44af8b8dcaa0297ac71b40822a6739ae6ea983de
AndreiYakerson <tubikwheels@gmail.com>
2025-07-02 21:54:14 +0300

    Add basic create container

--- apps/note/cmps/CreateContainer.jsx
+++ apps/note/cmps/CreateContainer.jsx
@@ -1,14 +1,27 @@
 
 
+const { useState } = React;
+
 export function CreateContainer() {
+
+    const [inputValue, setInputValue] = useState('hello');
+
+    function onHandleState(event) {
+        console.log(event.target.id);
+        event.target.classList.remove
+        
+    }
+
+
+
     return (
         <section className="create-container">
-            <input type="text" name="input" placeholder="type..." />
+            <input type="text" name="input" placeholder="Note..." />
             <div className="create-icons">
-            <button className="fa msg-icon"  onClick={() => console.log('click')}></button>
-            <button className="fa img-icon"></button>
-            <button className="fa film-icon"></button>
-            <button className="fa list-icon"></button>
+            <button id="txt" className="fa msg-icon" onClick={onHandleState}></button>
+            <button id="img" className="fa img-icon" onClick={onHandleState}></button>
+            <button id="film" className="fa film-icon" onClick={onHandleState}></button>
+            <button id="list" className="fa list-icon" onClick={onHandleState}></button>
             </div>
 
         </section>
