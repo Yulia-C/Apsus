29fb2a618575e35be8c238d1416433681a4347eb
Yulia C <yulka1355@gmail.com>
2025-07-02 16:33:16 +0300

    Change nav  mail and note links to svgs

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,21 @@
 const { Link, NavLink } = ReactRouterDOM
+// import { ReactComponent as GmailIcon } from '../assets/img/gmail.svg';
 
 export function AppHeader() {
 
-    return <header className="app-header">
+    return <header className="app-header container">
         <Link to="/">
-            <h3>LOGO ✨</h3>
-        </Link>
+            <img className="apsus-logo" src="assets/img/apsus-logo3.png"/>        
+            </Link>
         <nav>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
+            <NavLink to="/mail">
+                <img className="mail-logo" src="assets/img/gmail.svg" title="mail" />
+            </NavLink>
+            <NavLink to="/note">
+                <img className="note-logo" src="assets/img/keep.svg" title="keep" />
+            </NavLink>
         </nav>
     </header>
 }
