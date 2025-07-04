cae4c5c6e0067f3449e9e601dcae148fa52beb78
AndreiYakerson <tubikwheels@gmail.com>
2025-07-04 16:31:55 +0300

    Add basic cards preview

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -28,7 +28,7 @@ function NoteTxt({ note }) {
     return (
         <div className="note-txt card" >
             <p>{note.info.txt}</p>
-            
+
         </div>
     );
 }
@@ -36,15 +36,15 @@ function NoteTxt({ note }) {
 function NoteImg({ note }) {
     return (
         <div className="note-img card" >
-            <img src={note.info.url} alt={note.info.title} />
             <p>{note.info.title}</p>
+            <img src={note.info.url} />
         </div>
     );
 }
 
 function NoteTodos({ note }) {
     return (
-        <div className="note-todos card" style={note.style}>
+        <div className="note-todos card" >
             <h3>{note.info.title}</h3>
             <ul>
                 {note.info.todos.map((todo, idx) => (
