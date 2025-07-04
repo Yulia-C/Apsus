95b376667a41193f03ef3e80e4df40eaa1b7a0c8
AndreiYakerson <tubikwheels@gmail.com>
2025-07-04 16:30:51 +0300

    Add titles for note mode icons

--- apps/note/cmps/CreateContainer.jsx
+++ apps/note/cmps/CreateContainer.jsx
@@ -31,10 +31,10 @@ export function CreateContainer() {
             <input type="text" name="input" placeholder="Note..." />
 
             <div className="create-icons">
-            <button id="txt" className="fa txt-icon active" onClick={onHandleCreateMode}></button>
-            <button id="img" className="fa img-icon" onClick={onHandleCreateMode}></button>
-            <button id="film" className="fa film-icon" onClick={onHandleCreateMode}></button>
-            <button id="list" className="fa list-icon" onClick={onHandleCreateMode}></button>
+            <button id="txt" title="Text note" className="fa txt-icon active" onClick={onHandleCreateMode}></button>
+            <button id="img" title="Image note" className="fa img-icon" onClick={onHandleCreateMode}></button>
+            <button id="film" title="Video note" className="fa film-icon" onClick={onHandleCreateMode}></button>
+            <button id="list" title="Todos note" className="fa list-icon" onClick={onHandleCreateMode}></button>
             </div>
 
         </section>
