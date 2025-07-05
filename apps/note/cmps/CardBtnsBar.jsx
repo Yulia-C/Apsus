5b25ff8f761592bfe5a1e7017db7ca3189962bc9
AndreiYakerson <tubikwheels@gmail.com>
2025-07-05 17:57:16 +0300

    Add palette btn to card btns

--- apps/note/cmps/CardBtnsBar.jsx
+++ apps/note/cmps/CardBtnsBar.jsx
@@ -6,7 +6,7 @@ export function CardBtnsBar() {
         <div className="card-btns-container">
             <button className="btn btn-edit fa fa-pencil-square-o" title="Edit"></button>
             <button className="btn btn-pin fa fa-thumb-tack" title="Pin"></button>
-            <button className="btn btn-share fa fa-share-alt" title="Share"></button>
+            <button className="btn btn-share fa palette-icon" title="Share"></button>
             <button className="btn btn-delete fa fa-trash-o" title="Delete"></button>
     </div>
     )
