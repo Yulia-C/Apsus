9bb1c5785526f679cc8c637ce8f32b89ab75732c
Yulia C <yulka1355@gmail.com>
2025-07-06 20:16:13 +0300

    Fix typo + mailFilter&List styling touches

--- apps/mail/cmps/MailMenu.jsx
+++ apps/mail/cmps/MailMenu.jsx
@@ -1,7 +1,7 @@
 export function MailMenu() {
     return (
         <section className="mail-menu flex column">
-           
+
             <i className="icon outlined compose" />
             <i className="icon outlined inbox" />
             <i className="icon outlined star" />
