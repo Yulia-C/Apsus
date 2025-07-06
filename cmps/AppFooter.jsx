035e4a6c097c158b0d7432109306f079c48f2287
Yulia C <yulka1355@gmail.com>
2025-07-06 23:58:33 +0300

    Create & design footer cmp

---
+++ cmps/AppFooter.jsx
@@ -0,0 +1,9 @@
+export function AppFooter() {
+    return (
+        <footer className="flex column align-center">
+            <img className="apsus-logo" src="assets/img/apsus-logo3.png" />
+            <p>© All rights reserved - Andrei Yakerson & Yulia Chernyak</p>
+        </footer>
+    )
+
+}
