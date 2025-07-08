c1979bb89dff241e699520aaddcf01c8c86e6cf5
Yulia C <yulka1355@gmail.com>
2025-07-08 23:35:57 +0300

    Add required pattern for email

--- apps/mail/cmps/MailEdit.jsx
+++ apps/mail/cmps/MailEdit.jsx
@@ -36,9 +36,11 @@ export function MailEdit({ isModalOpen, onCloseModal }) {
     function handleChange({ target }) {
         const field = target.name
         let value = target.value
+        if (value === 'to')
+            if (target.type === 'checkbox') value = target.checked
+            else if (target.type === 'range') value = +value
 
-        if (target.type === 'checkbox') value = target.checked
-        else if (target.type === 'range') value = +value
+
 
         setMailToEdit(prevMail => ({ ...prevMail, [field]: value }))
     }
@@ -47,23 +49,23 @@ export function MailEdit({ isModalOpen, onCloseModal }) {
     if (!isModalOpen) return null
 
     return (
-              <div className={`${isModalOpen ? 'open' : ''} compose-modal`}>
+        <div className={`${isModalOpen ? 'open' : ''} compose-modal`}>
 
-         <form onSubmit={onSendMail}>
-             <div className="compose-header flex space-between align-center">
-                 <p>New Message</p>
-             </div>
-             <button onClick={onCloseModal} type="button" className="close-btn">X</button>
-             <label className="label-to" htmlFor="to">To:</label>
-             <input onChange={handleChange} className="input-to" value={to} type="text" id="to" name="to" required />
+            <form onSubmit={onSendMail}>
+                <div className="compose-header flex space-between align-center">
+                    <p>New Message</p>
+                </div>
+                <button onClick={onCloseModal} type="button" className="close-btn">X</button>
+                <label className="label-to" htmlFor="to">To:</label>
+                <input onChange={handleChange} className="input-to" value={to} type="email" id="to" name="to" pattern='/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$/i' required />
 
-             <label className="label-sub" htmlFor="subject">Subject</label>
-             <input onChange={handleChange} className="input-sub flex align-center" value={subject} type="text" id="subject" name="subject" required/>
-             <textarea onChange={handleChange} className="compose-body" value={body} type="text" id="body" name="body" required/>
+                <label className="label-sub" htmlFor="subject">Subject</label>
+                <input onChange={handleChange} className="input-sub flex align-center" value={subject} type="text" id="subject" name="subject" required />
+                <textarea onChange={handleChange} className="compose-body" value={body} type="text" id="body" name="body" required />
 
-             <button type="submit" className="send-btn">Send</button>
-         </form>
-     </div>
-       
+                <button type="submit" className="send-btn">Send</button>
+            </form>
+        </div>
+
     )
 }
