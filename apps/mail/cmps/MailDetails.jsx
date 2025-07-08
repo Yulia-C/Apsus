234c6e3638a2739d09cd9282577f0ebeea8de8ab
Yulia C <yulka1355@gmail.com>
2025-07-08 22:57:28 +0300

    Change location of MailDetails

--- apps/mail/cmps/MailDetails.jsx
+++ apps/mail/cmps/MailDetails.jsx
@@ -2,7 +2,7 @@ import { mailService } from "../services/mail.service.js"
 import { showErrorMsg, showSuccessMsg } from "../../../services/event-bus.service.js";
 
 const { useState, useEffect } = React
-const { useParams, useNavigate} = ReactRouterDOM
+const { useParams, useNavigate } = ReactRouterDOM
 
 export function MailDetails({ onMoveToTrash, onReply, onToggleRead }) {
 
@@ -17,7 +17,7 @@ export function MailDetails({ onMoveToTrash, onReply, onToggleRead }) {
     function loadMail() {
         mailService.get(mailId)
             .then(mail => {
-                mail = { ...mail, isRead: true }
+                mail = { ...mail, isRead: !isRead }
                 mailService.save(mail)
                 setMail(mail)
             })
@@ -27,37 +27,37 @@ export function MailDetails({ onMoveToTrash, onReply, onToggleRead }) {
             })
     }
 
-    function onBack() {
-        navigate('/mail')
-    }
+function onBack() {
+    navigate('/mail')
+}
 
-    if (!mail) return <div>Loading...</div>
+if (!mail) return <div>Loading...</div>
 
-    return (
+return (
 
-        <section className="mail-details">
-            <section className="flex row">
-                <section>
+    <section className="mail-details">
+        <section className="flex row">
+            <section>
 
-                    <div className="mail-d-actions flex space-between">
-                        <div onClick={onBack}><i title="back" className="icon outlined arrow-back" /></div>
-                        <div>
-                            <i onClick={() => onReply(mailId)} title="reply" className="icon outlined reply" />
-                            <i onClick={() => onToggleRead(mailId)} title="mark-unread" className="icon outlined mark-unread" />
-                            <i onClick={() => {
-                                onMoveToTrash(mailId)
-                                setTimeout(onBack, 500)
-                            }} title="send to trash" className="icon outlined delete" />
-                        </div>
+                <div className="mail-d-actions flex space-between">
+                    <div onClick={onBack}><i title="back" className="icon outlined arrow-back" /></div>
+                    <div>
+                        <i onClick={() => onReply(mailId)} title="reply" className="icon outlined reply" />
+                        <i onClick={()=>onToggleRead(mailId)} title="mark-unread" className="icon outlined mark-unread" />
+                        <i onClick={() => {
+                            onMoveToTrash(mailId)
+                            setTimeout(onBack, 500)
+                        }} title="send to trash" className="icon outlined delete" />
                     </div>
-                    <div className="mail-details-content container">
-                        <p className="mail-subject-content">Subject: {mail.subject}</p>
-                        <p className="mail-from-content">From: {mail.from}</p>
-                        <p className="mail-to-content">To: {mail.to}</p>
-                        <p className="mail-body-content">{mail.body}</p>
-                    </div>
-                </section>
+                </div>
+                <div className="mail-details-content container">
+                    <p className="mail-subject-content">Subject: {mail.subject}</p>
+                    <p className="mail-from-content">From: {mail.from}</p>
+                    <p className="mail-to-content">To: {mail.to}</p>
+                    <p className="mail-body-content">{mail.body}</p>
+                </div>
             </section>
         </section>
-    )
+    </section>
+)
 }
