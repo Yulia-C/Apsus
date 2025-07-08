1230effa52a51337e123e5a22e1f3e72cdeca3a9
Yulia C <yulka1355@gmail.com>
2025-07-08 14:59:31 +0300

    Implement dynamic 'active class in side-menu

--- apps/mail/cmps/MailMenu.jsx
+++ apps/mail/cmps/MailMenu.jsx
@@ -1,13 +1,30 @@
 export function MailMenu() {
-    return (
-        <section className="mail-menu aside flex column">
+const [activeItem, setActiveItem] = useState('inbox')
+    const itemsMap = [
+        { key: 'inbox' },
+        { key: 'star' },
+        { key: 'sent' },
+        { key: 'draft' },
+        { key: 'delete' },
+    ]
 
-            <i className="icon outlined compose" />
-            <i className="icon outlined inbox" />
-            <i className="icon outlined star" />
-            <i className="icon outlined sent" />
-            <i className="icon outlined draft" />
-            <i className="icon outlined delete" />
+    return (
+        <section className={`${isMenuOpen ? 'open' : ''} mail-menu aside flex column`}>
+            <i title="compose" className="icon outlined compose active" />
+            {itemsMap.map(item => {
+                const isActive = activeItem === item.key
+
+                return isActive ? (<img onClick={() => setActiveItem(item.key)} key={item.key} title={item.key}
+                    className={`m-${item.key} active`} src={`assets/icons/m-${item.key}.svg`} />)
+                    : (<i key={item.key} title={item.key}
+                        className={`icon outlined ${item.key}`}
+                        onClick={() => setActiveItem(item.key)} />)
+            })}
+            {/* <i title="inbox" className="icon outlined inbox" />
+            <i title="star" className="icon outlined star" />
+            <i title="sent" className="icon outlined sent" />
+            <i title="draft" className="icon outlined draft" />
+            <i title="delete" className="icon outlined delete" /> */}
         </section>
     )
 }
