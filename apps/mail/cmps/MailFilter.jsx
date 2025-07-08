cde6777759016f31e728974a540edf90b1e1a061
Yulia C <yulka1355@gmail.com>
2025-07-08 14:55:28 +0300

    Separate mailFilter to MailSort & MailFilter for layout purposes

--- apps/mail/cmps/MailFilter.jsx
+++ apps/mail/cmps/MailFilter.jsx
@@ -1,7 +1,7 @@
 import { mailService } from "../services/mail.service.js"
 const { useEffect, useState } = React
 
-export function MailFilter({ defaultFilter, onSetFilterBy }) {
+export function MailFilter({ defaultFilter, onSetFilterBy, onToggleMenu }) {
 
     const [filterByToEdit, setFilterByToEdit] = useState({ ...defaultFilter })
     const { txt } = filterByToEdit
@@ -32,33 +32,67 @@ export function MailFilter({ defaultFilter, onSetFilterBy }) {
         setFilterByToEdit(prevFilter => ({ ...prevFilter, [field]: value }))
     }
 
+
     return (
         <section className="mail-search m-header">
-            {/* <div className="flex column"> */}
-                    <i className="toggle-menu icon outlined menu"></i>
-                    <img className="mail-logo" src="assets/img/gmail.svg" title="mail" />
-                {/* <div className="form-container"> */}
-                    {/* <div onChange={handleChange} className="search-bar"> */}
-                        <form onChange={handleChange} className="search-bar">
-                            <i className="icon outlined search"></i>
-                            <input onChange={handleChange} value={txt} id='txt' name='txt' type='text' placeholder="Search mail" />
-                        </form>
-                    {/* </div> */}
-                <form className="mail-filter">
-                    <select id="isRead" onChange={handleChange} className="isRead" name="isRead">
-                        <option value="">All mails</option>
-                        <option value={true}>Read</option>
-                        <option value={false}>Unread</option>
-                    </select>
+            <i onClick={onToggleMenu} className="toggle-menu icon outlined menu"></i>
+            <img className="mail-logo" src="assets/img/gmail.svg" title="mail" />
 
-                    <select id="isStarred" name="isStarred" onChange={handleChange}>
-                        <option value="">⭐ All mails</option>
-                        <option value="true">Starred</option>
-                        <option value="false">Unstarred</option>
-                    </select>
-                </form>
-                {/* </div> */}
-            {/* </div> */}
+            <form onChange={handleChange} className="search-bar">
+                <i className="icon outlined search"></i>
+                <input onChange={handleChange} value={txt} id='txt' name='txt' type='text' placeholder="Search mail" />
+            </form>
+
         </section>
+
+    )
+}
+
+export function MailSort({ defaultFilter, onSetFilterBy }) {
+    const [filterByToEdit, setFilterByToEdit] = useState({ ...defaultFilter })
+    const { txt } = filterByToEdit
+
+    useEffect(() => {
+        onSetFilterBy(filterByToEdit)
+    }, [filterByToEdit])
+
+    function handleChange({ target }) {
+        const field = target.name
+        let value = target.value
+        switch (target.type) {
+
+            case 'range':
+                value = +value
+                break;
+
+            case 'checkbox':
+                value = target.checked
+                break
+        }
+
+        if (field === 'isRead' || field === 'isStarred') {
+            if (value === 'true') value = true
+            else if (value === 'false') value = false
+            else if (value = '') value = null
+        }
+        setFilterByToEdit(prevFilter => ({ ...prevFilter, [field]: value }))
+    }
+    return (
+        <section className="mail-filter">
+            <form className="flex row">
+                <select id="isRead" onChange={handleChange} className="isRead" name="isRead">
+                    <option value="">All mails</option>
+                    <option value={true}>Read</option>
+                    <option value={false}>Unread</option>
+                </select>
+
+                <select id="isStarred" name="isStarred" onChange={handleChange}>
+                    <option value="">⭐ All mails</option>
+                    <option value="true">Starred</option>
+                    <option value="false">Unstarred</option>
+                </select>
+            </form>
+        </section>
+
     )
 }
