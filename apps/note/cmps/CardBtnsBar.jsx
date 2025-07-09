3cf010254cfac77820f461f5e811f7e7d4af2539
AndreiYakerson <tubikwheels@gmail.com>
2025-07-09 19:55:09 +0300

    Fix card btns icons

--- apps/note/cmps/CardBtnsBar.jsx
+++ apps/note/cmps/CardBtnsBar.jsx
@@ -4,10 +4,10 @@ export function CardBtnsBar() {
     return (
 
         <div className="card-btns-container">
-            <button className="btn btn-edit fa fa-pencil-square-o" title="Edit"></button>
-            <button className="btn btn-pin fa fa-thumb-tack" title="Pin"></button>
+            <button className="btn btn-edit fa edit-icon" title="Edit"></button>
+            <button className="btn btn-pin fa pin-icon" title="Pin"></button>
             <button className="btn btn-share fa palette-icon" title="Share"></button>
-            <button className="btn btn-delete fa fa-trash-o" title="Delete"></button>
+            <button className="btn btn-delete fa delete-icon" title="Delete"></button>
     </div>
     )
 }
