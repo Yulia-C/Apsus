6800db083a57c0746aa3cb1a75fc64b4f75bc9fb
Yulia C <yulka1355@gmail.com>
2025-07-09 10:48:37 +0300

    Remove mark unread icon from mail details

--- apps/mail/cmps/MailDetails.jsx
+++ apps/mail/cmps/MailDetails.jsx
@@ -17,7 +17,7 @@ export function MailDetails({ onMoveToTrash, onReply, onToggleRead }) {
     function loadMail() {
         mailService.get(mailId)
             .then(mail => {
-                mail = { ...mail, isRead: !isRead }
+                mail = { ...mail, isRead: true }
                 mailService.save(mail)
                 setMail(mail)
             })
@@ -27,37 +27,36 @@ export function MailDetails({ onMoveToTrash, onReply, onToggleRead }) {
             })
     }
 
-function onBack() {
-    navigate('/mail')
-}
+    function onBack() {
+        navigate('/mail')
+    }
 
-if (!mail) return <div>Loading...</div>
+    if (!mail) return <div>Loading...</div>
 
-return (
+    return (
 
-    <section className="mail-details">
-        <section className="flex row">
-            <section>
+        <section className="mail-details">
+            <section className="flex row">
+                <section>
 
-                <div className="mail-d-actions flex space-between">
-                    <div onClick={onBack}><i title="back" className="icon outlined arrow-back" /></div>
-                    <div>
-                        <i onClick={() => onReply(mailId)} title="reply" className="icon outlined reply" />
-                        <i onClick={()=>onToggleRead(mailId)} title="mark-unread" className="icon outlined mark-unread" />
-                        <i onClick={() => {
-                            onMoveToTrash(mailId)
-                            setTimeout(onBack, 500)
-                        }} title="send to trash" className="icon outlined delete" />
+                    <div className="mail-d-actions flex space-between">
+                        <div onClick={onBack}><i title="back" className="icon outlined arrow-back" /></div>
+                        <div>
+                            <i onClick={() => onReply(mailId)} title="reply" className="icon outlined reply" />                           
+                            <i onClick={() => {
+                                onMoveToTrash(mailId)
+                                setTimeout(onBack, 500)
+                            }} title="send to trash" className="icon outlined delete" />
+                        </div>
                     </div>
-                </div>
-                <div className="mail-details-content container">
-                    <p className="mail-subject-content">Subject: {mail.subject}</p>
-                    <p className="mail-from-content">From: {mail.from}</p>
-                    <p className="mail-to-content">To: {mail.to}</p>
-                    <p className="mail-body-content">{mail.body}</p>
-                </div>
+                    <div className="mail-details-content container">
+                        <p className="mail-subject-content">Subject: {mail.subject}</p>
+                        <p className="mail-from-content">From: {mail.from}</p>
+                        <p className="mail-to-content">To: {mail.to}</p>
+                        <p className="mail-body-content">{mail.body}</p>
+                    </div>
+                </section>
             </section>
         </section>
-    </section>
-)
+    )
 }
