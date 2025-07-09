42a33be83ece6d79f09d6ff85b8457a699eff6b4
Yulia C <yulka1355@gmail.com>
2025-07-09 22:01:12 +0300

    Create styling & layout for home page and about

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,47 @@
 
 export function About() {
-    return <section className="container about">
-        <h1>About Page</h1>
+    return <section className="about-container">
+        {/* <section className="flex  column align-center"> */}
+
+        <h1> Our Team </h1>
+        <div className="about-box flex space-between">
+
+            <div className="about-card">
+                <h1>Yulia Chernyak</h1>
+                <img src="assets/img/yulia.png" alt="Yulia Chernyak" title="Yulia Chernyak" />
+                <p>
+                    Fullstack development student in Coding Academy with a keen eye for detail
+                    and user experience.
+                </p>
+                <p> - Apsus Mail app -</p>
+                <a
+                    href="https://github.com/Yulia-C"
+                    target="_blank"
+                    title="go to github"
+                >
+                    <img className="git-logo" src="assets/img/github.png" title="git link" alt="git logo" />
+                </a>
+            </div>
+
+            <div className="about-card">
+                <h1>Andrei Yakerson</h1>
+                <img src="assets/img/andrey.png" alt="Andrei Yakerson" title="Andrei Yakerson" />
+                <p>
+                    I am a full stack development student who enjoys solving
+                    problems through clean and
+                    efficient code.
+                </p>
+                <p> - Apsus Keep app -</p>
+
+                <a
+                    href="https://github.com/AndreiYakerson"
+                    target="_blank"
+                    title="go to github"
+                >
+                    <img className="git-logo" src="assets/img/github.png" title="git link" alt="git logo" />
+                </a>
+            </div>
+        </div>
     </section>
+    // </section>
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,49 @@
 import { showSuccessMsg } from '../services/event-bus.service.js'
+const { Link, NavLink } = ReactRouterDOM
 
 export function Home() {
-    return <section className="container home">
-        <h1>Welcome home</h1>
-        <button onClick={() => showSuccessMsg('Yep, that works')}>Show Msg</button>
-        <div className="box-container">
-            <div className="box1"></div>
-            <div className="box2"></div>
-        </div>
-    </section>
+    return (
+
+        <section className="home-container ">
+
+            <p className="p-welcome">
+                Welcome to APSUS <br />
+                Your all-in-one personal productivity space. <br />
+                Stay organized, focused, and connected with <br />
+                two powerful tools:<br />
+            </p>
+
+            <div className="box-container">
+
+                <section className="mail-card flex column align-center">
+                    <h1> APSUS Mail</h1>
+                    <p>
+                        Manage your emails with ease.
+                        Compose, read, and organize your messages.
+                        Enjoy a simple, fast, and intuitive interface.
+                    </p>
+                    <img className="mail-logo" src="assets/img/gmail.svg" title="mail" />
+                    <NavLink to="/mail">
+                        <button>Go</button>
+                    </NavLink>
+                </section>
+
+                <section className="note-card flex column align-center">
+                    <h1>APSUS Keep</h1>
+                    <p>
+                        Create text notes with colors and tags.
+                        Pin important notes to keep on top.
+                        Easily search and manage your thoughts
+                    </p>
+                    <img className="note-logo" src="assets/img/keep.svg" title="keep" />
+                    <NavLink to="/note">
+                        <button>Go</button>
+                    </NavLink>
+                </section>
+            </div>
+
+        
+            {/* {/* <button onClick={() => showSuccessMsg('Yep, that works')}>Show Msg</button> */}
+        </section >
+    )
 }
