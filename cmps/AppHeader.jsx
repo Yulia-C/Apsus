b5436557d7e9e1007c08700d0115a50820f26f15
Yulia C <yulka1355@gmail.com>
2025-07-09 21:59:21 +0300

    Improve app header css

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -5,17 +5,17 @@ export function AppHeader() {
 
     return <header className="app-header container">
         <Link to="/">
-            <img className="apsus-logo" src="assets/img/apsus-logo3.png"/>        
-            </Link>
-        <nav>
+            <img className="apsus-logo" src="assets/img/apsus-logo3.png" />
+        </Link>
+        <nav className="nav-bar">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">
-                <img className="mail-logo" src="assets/img/gmail.svg" title="mail" />
-            </NavLink>
-            <NavLink to="/note">
-                <img className="note-logo" src="assets/img/keep.svg" title="keep" />
-            </NavLink>
+                <NavLink to="/mail">
+                    <img className="mail-logo" src="assets/img/gmail.svg" title="mail" />
+                </NavLink>
+                <NavLink to="/note">
+                    <img className="note-logo" src="assets/img/keep.svg" title="keep" />
+                </NavLink>
         </nav>
     </header>
 }
